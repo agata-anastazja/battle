a5b5aeafeb835dfb7eb4d470301b363ae88a28fa
Agata Sumowska <agata.sumowska@gmail.com>
2016-11-23 17:53:22 +0000

    refactoring

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,14 +3,31 @@ require 'game'
 
 describe Game do
 
-  subject(:game) {described_class.new}
-  let(:player) {double :player, receive_damage: nil}
+  let(:agata) {double :player, name: "Agata"}
+  let(:chris) {double :player, name: "Chris"}
+  subject(:game) {described_class.new(agata, chris)}
+
+
 
   describe '#attack' do
     it 'attacks a player' do
-      expect(player).to receive(:receive_damage)
-      game.attack(player)
+      expect(chris).to receive(:receive_damage)
+      game.attack(chris)
     end
+  end
+
+  describe '#initialize' do
+    it "initializes 2 players" do
+
+      expect(game.player_1).to eq agata
+    end
+
+    it "initializes 2 players" do
+
+      expect(game.player_2).to eq chris
+    end
+
+
   end
 
 end
