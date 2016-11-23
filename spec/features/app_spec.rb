01a9c5a14582cfa049539480c728732d78314411
Teako Warfield-Graham <teakowg@gmail.com>
2016-11-23 14:52:54 +0000

    First test passing

---
+++ spec/features/app_spec.rb
@@ -0,0 +1,10 @@
+
+RSpec.feature "Fill in form with name" do
+  scenario "User fills out name in form" do
+    visit("/")
+    fill_in :player_1, with: 'Tom'
+    fill_in :player_2, with: 'Dan'
+    click_button 'Submit'
+    expect(page).to have_content 'Tom vs. Dan'
+  end
+end
