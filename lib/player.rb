9d52d9edcc9df57ca1cfa00eea84ddc4c4039a49
Teako Warfield-Graham <teakowg@gmail.com>
2016-11-24 12:24:43 +0000

    Wrote hit points method

--- lib/player.rb
+++ lib/player.rb
@@ -1,9 +1,14 @@
 class Player
 
-  attr_reader :name
+  attr_reader :name, :hit_points
 
   def initialize(name)
     @name = name
+    @hit_points = 100
+  end
+
+  def reduce_hit_points
+    @ hit_points -= 10
   end
 
 
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,10 +1,21 @@
 require 'player'
 
 describe Player do
+
   subject(:player){ described_class.new( name ) }
   let(:name){double :name}
+
   it "returns it's own name" do
-  expect(player.name).to eq name
+    expect(player.name).to eq name
   end
+
+  it 'should initialize with 100 hit points' do
+    expect(player.hit_points).to eq(100)
+  end
+
+  it 'reduces player_2 points by 10' do
+    expect(player.reduce_hit_points).to eq(90)
+  end
+
 
 end
