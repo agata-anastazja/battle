5ec6a9519c3503c484c833f39968801992763815
TudorTacal <tudortacal@gmail.com>
2016-11-24 11:23:18 +0000

    Challenge 23, separating logic to model

--- app.rb
+++ app.rb
@@ -10,18 +10,22 @@ class Battle < Sinatra::Base
   end
 
   get '/play' do
+    @player_1 = $player_1.name
+    @player_2 = $player_2.name
     erb :play
   end
 
   post '/battle-names' do
+    p params
     $player_1 = Player.new(params[:player_1])
     $player_2 = Player.new(params[:player_2])
-    # @player_1 = params[:player_1]
-    # @player_2 = params[:player_2]
+
     redirect '/play'
   end
 
   get '/attack' do
+    @player_1 = $player_1.name
+    @player_2 = $player_2.name
     erb :attack
   end
 
--- spec/features/app_spec.rb
+++ spec/features/app_spec.rb
@@ -8,13 +8,19 @@ RSpec.feature "Battle" do
 
   scenario "User can see player 2 hit points" do
     sign_in_and_play
-    expect(page).to have_content 'Dan: 50 points'
+    expect(page).to have_content 'Dan: 100/100 points'
   end
 
   scenario "User gets confirmation of attack" do
     sign_in_and_play
     click_button 'ATTACK!'
     expect(page).to have_content ('Tom has attacked Dan!')
+  end
+
+  scenario "Players 2 points reduced upon attack" do
+    sign_in_and_play
+    click_button 'ATTACK!'
+    expect(page).to have_content "Dan 90/100 points"
   end
 
 end
